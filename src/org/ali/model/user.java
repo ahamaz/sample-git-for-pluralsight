0d2e1a3599a2a509a29f392bae0aa1f4fcce1d09
ahamaz <ahasanfsm@hotmail.com>
2020-04-26 01:11:37 +0100

    added getter and setterfor the name field

--- src/org/ali/model/user.java
+++ src/org/ali/model/user.java
@@ -3,4 +3,14 @@ package org.ali.model;
 public class user {
 	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
 }
